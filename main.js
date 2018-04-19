c5ff7d15ad03f521e1d55e42007ff572d69a7a07
alex <alex.kozhukhov.201@gmail.com>
2018-04-19 19:43:59 +0300

    Added get, post, delete

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
-$(document).ready(function(){ 
-    $("#categories").click(function(e){
-alert("1");
+function loadCategories() {
+  $.ajax({
+    url: "http://localhost:2403/categories",
+    type: "GET",
+    success: function(data) {
+      $("table tbody").html("");
+      for (var i = 0; i < data.length; i++) {
+        $("table tbody").append(
+          "<tr data-id="+data[i].id +"><td>" + (i + 1) + "</td><td>" + data[i].name + '</td><td><button type="button" class="btn btn-danger">delete</button></td></tr>'
+        );
+      }
+    }
+  });
+}
+
+function setupCategoriesPage() {
+  $("form").submit(function(e) {
+    e.preventDefault();
+    var text = $("#exampleInputText").val();
+    e.target.children[0].children[1].value = " ";
+    $.ajax({
+      url: "http://localhost:2403/categories",
+      data: { name: text },
+      type: "POST",
+      success: function() {
+        loadCategories();
+      }
     });
-});         
+  });
+}
+function deleteCategory(a) {
+      $.ajax({
+        url: "http://localhost:2403/categories/"+a,
+        type: "DELETE",
+        success: function() {
+          loadCategories();
+        }
+      });
+};
+function init() {
+  setupCategoriesPage();
+  loadCategories();
+}
+$(document).ready(function() {
+  init();
+  $("table").on("click","button", function(e){
+    var c = $(e.target).parents('tr');
+    deleteCategory(c.data('id'));
+});
+});
