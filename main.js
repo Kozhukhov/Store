d364b3963ab662da979ccba839c68604a594a1df
alex <alex.kozhukhov.201@gmail.com>
2018-04-22 16:33:55 +0300

    Added Products and good design

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
 var newText;
+// Список всех категорий
 function loadCategories() {
   $.ajax({
     url: "http://localhost:2403/categories",
     type: "GET",
     success: function(data) {
-      $("table tbody").html("");
+      $("#all-main table tbody").html("");
       for (var i = 0; i < data.length; i++) {
-        $("table tbody").append(
-          "<tr data-id="+data[i].id +"><td>" + (i + 1) + "</td><td>" + data[i].name + '</td><td><button type="button" class="btn btn-danger">delete</button></td><td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">edit</button></td></tr>'
+        $("#all-main table tbody").append(
+          "<tr data-id="+data[i].id +"><td>" + (i + 1) + "</td><td>" + data[i].name + '</td><td><button type="button" class="close cl1" aria-label="Close"><span aria-hidden="true">&times;</span></button><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">edit</button></td></tr>'
         );
       }
     }
   });
 }
-
+//Список всех продуктов
+function loadProducts() {
+  $.ajax({
+    url: "http://localhost:2403/products",
+    type: "GET",
+    success: function(data) {
+      $("#all-main table tbody").html("");
+      for (var i = 0; i < data.length; i++) {
+        $("#all-main table tbody").append(
+          "<tr data-id="+data[i].id +"><td>" + (i + 1) + "</td><td>" + data[i].name + '</td><td><button type="button" class="close cl2" aria-label="Close"><span aria-hidden="true">&times;</span></button><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">edit</button></td></tr>'
+        );
+      }
+    }
+  });
+}
+//Добавление категории
 function setupCategoriesPage() {
-  $("form").submit(function(e) {
-    var text = $(".form-group input").val();
+  $("#all-main #formCategory").submit(function(e) {
+    var text1 = $("#formCategory .form-group input").val();
+    console.log(text1);
     e.preventDefault();
     e.target.children[0].children[1].value = " ";
     $.ajax({
       url: "http://localhost:2403/categories",
-      data: { name: text },
+      data: { name: text1 },
       type: "POST",
       success: function() {
         loadCategories();
@@ -29,6 +46,23 @@ function setupCategoriesPage() {
     });
   });
 }
+//Добавление продукта
+function setupProductsPage() {
+  $("#all-main #formProduct").submit(function(e) {
+    var text2 = $("#formProduct .form-group input").val();
+    e.preventDefault();
+    e.target.children[0].children[1].value = " ";
+    $.ajax({
+      url: "http://localhost:2403/products",
+      data: { name: text2, price: 20, description: text2},
+      type: "POST",
+      success: function() {
+        loadProducts();
+      }
+    });
+  });
+}
+//Запрос на удаление категории
 function deleteCategory(a) {
       $.ajax({
         url: "http://localhost:2403/categories/"+a,
@@ -38,7 +72,30 @@ function deleteCategory(a) {
         }
       });
 };
-
+//Запрос на удаление продукта
+function deleteProduct(a) {
+  $.ajax({
+    url: "http://localhost:2403/products/"+a,
+    type: "DELETE",
+    success: function() {
+      loadProducts();
+    }
+  });
+};
+//Обработка кнопки удаление категории
+ function clickDeleteCategory(){
+  $("table").on("click",".cl1", function(e){
+    var c = $(e.target).parents('tr');
+    deleteCategory(c.data('id'));
+});
+ }
+ //Обработка кнопки удаление продукта
+ function clickDeleteProduct(){
+  $("table").on("click",".cl2", function(e){
+    var c = $(e.target).parents('tr');
+    deleteProduct(c.data('id'));
+});
+ }
 function changeCategory(a, text) {
   $.ajax({
     url: "http://localhost:2403/categories/"+a,
@@ -50,23 +107,43 @@ function changeCategory(a, text) {
   });
 };
 
-function init() {
+function init1() {
   setupCategoriesPage();
   loadCategories();
 }
-$(document).ready(function() {
-  init();
-
-
-
-  $("table").on("click",".btn-danger", function(e){
-    var c = $(e.target).parents('tr');
-    deleteCategory(c.data('id'));
+function init2() {
+  setupProductsPage();
+  loadProducts();
+}
+function showCategories(){
+  $("#categories").click(function(){
+  $(".all-main").hide();
+  $("#all-main").show();
+  $("#formCategory").show();
+  $("#formProduct").hide();
+  init1();
 });
+}
+function showProducts(){
+  $("#products").click(function(){
+  $(".all-main").hide();
+  $("#all-main").show();
+  $("#formCategory").hide();
+  $("#formProduct").show();
+  init2();
+});
+}
+
+$(document).ready(function() {
+  showCategories();
+  showProducts();
+  clickDeleteCategory();
+  clickDeleteProduct();
+
+
 var c;
 $("table").on("click",".btn-primary", function(event){
 c = $(event.target).parents('tr');
-console.log("blaaaaaa");
   $("main").one("click",".save", function(){
     newText = $(".modal-body input").val();
     changeCategory(c.data('id'),newText);
